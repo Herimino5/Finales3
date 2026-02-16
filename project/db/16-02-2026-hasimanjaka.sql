0dac14bffabaa5e5c91e1a2cc3ff30424da5e80a
Hasiman <hasimanjakaditra2809@gmail.com>
2026-02-16 18:35:49 +0300

    [fix] sql

--- project/db/16-02-2026-hasimanjaka.sql
+++ project/db/16-02-2026-hasimanjaka.sql
@@ -5,13 +5,8 @@ CREATE TABLE IF NOT EXISTS s3fin_distribution (
     don_id INT NOT NULL,
     quantite_distribuee INT NOT NULL,
     date_distribution TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    statut VARCHAR(50) DEFAULT 'En cours',
+ 
     FOREIGN KEY (besoin_id) REFERENCES s3fin_besoin(id),
     FOREIGN KEY (don_id) REFERENCES s3fin_don(id)
-);
+    );
 
--- Ajouter une colonne pour le statut dans la table besoin si elle n'existe pas
-ALTER TABLE s3fin_besoin ADD COLUMN IF NOT EXISTS statut VARCHAR(50) DEFAULT 'En attente';
-
--- Ajouter une colonne pour le statut dans la table don si elle n'existe pas  
-ALTER TABLE s3fin_don ADD COLUMN IF NOT EXISTS statut VARCHAR(50) DEFAULT 'Disponible';
