76c6961d516808d3caf7f36030be1e99f2caf4cd
AndriantsehenoAnja <andriantsehenoanja926@gmail.com>
2026-02-16 15:18:29 +0300

    dashbord controller and model besoin

---
+++ project/app/models/BesoinModel.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\models;
+
+class BesoinModel {
+    private $db;
+
+    public function __construct($pdo) {
+        $this->db = $pdo;
+    }
+
+    /**
+     * Récupère la liste des besoins et des dons pour chaque ville à partir de la vue.
+     */
+    public function getBesoinDonsParVille() {
+        $sql = "SELECT * FROM v_ville_besoins_dons";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
